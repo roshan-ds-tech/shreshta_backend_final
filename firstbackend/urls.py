7bcf9ee6cd8399cf73271cc0849ea8bc74499042
Roshan DS <roshands00270@gmail.com>
2025-12-17 05:25:15 +0530

    Fix allowed hosts for Render deployment

--- firstbackend/urls.py
+++ firstbackend/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"status": "backend running", "service": "Shreshta API"})
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
